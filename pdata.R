47de7bad815df9adfd7c7c6a8f26e604e846201e
進撃の雯雯老師 <askahu6@gmail.com>
2020-02-29 20:19:02 +0800

    Update pdata.R

--- pdata.R
+++ pdata.R
@@ -7,3 +7,31 @@ library(dplyr)
 library(openxlsx)
 library(lubridate)
 library(sqldf)
+
+#Loading
+
+idolmap <- read.xlsx('person.xlsx')
+files <- paste0('data',c(2001,2002),'.csv')
+system.time(
+  raw <- lapply(files,function(x){
+    print(paste('Loading',x,'at',Sys.time()))
+    x <- select(fread(x),user=vcuser,idol=person,vote=check,time_raw=updated) %>% 
+    mutate(time=as.POSIXct(gsub('T',' ',substr(time_raw,1,19))) + 3600*8,
+           year=year(time),month=month(time),day=day(time),hour=hour(time)) %>%
+           mutate(day=year*10000+month*100+day) %>%
+           select(-time_raw) 
+  })
+)
+X <- do.call(rbind,raw)
+mem <- X %>% group_by(user,day) %>% 
+              summarise(vote=sum(vote)) %>% 
+              filter(vote>1) %>% 
+              arrange(user,day) %>% 
+              mutate(mem=1)
+
+
+
+
+
+
+
